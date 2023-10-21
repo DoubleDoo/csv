3f0cbcc298e73a11751527f4d11c27663f36af53
Dmitry Dubina <dubinich@pop-os.localdomain>
2023-10-21 16:22:38 +0300

    fix user and admin creation

--- back/app/database/database.py
+++ back/app/database/database.py
@@ -3,6 +3,7 @@ from .models.file import File
 from .models.role import Role
 from .base import Base
 from flask_sqlalchemy import SQLAlchemy
+import time
 
 db = SQLAlchemy(model_class=Base)
 
@@ -15,25 +16,41 @@ def create_database(app):
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()
-
-        obj = db.session.execute(
-            db.select(User).where(User.role == Role.ADMIN)
-        ).scalar()
-        # ___________________________________________________________________________
-        if not obj:
-            obj = User(
-                name="ivan",
-                surname="ivanov",
-                email="ivan@ivanov.ru",
-                password="ivan",
-                role=Role.ADMIN,
-            )
-            db.session.add(obj)
-            db.session.commit()
-            obj = obj.to_json()
-            print("_______________________________________________")
-            print("ADMIN entity created")
-            print("login: ivan@ivanov.ru")
-            print("password: ivan")
-            print("_______________________________________________")
+        tries = 5
+        while tries > 0:
+            try:
+                db.create_all()
+                obj = db.session.execute(
+                    db.select(User).where(User.role == Role.ADMIN)
+                ).scalar()
+                if not obj:
+                    obj = User(
+                        name="admin",
+                        surname="admin",
+                        email="admin@admin.ru",
+                        password="admin",
+                        role=Role.ADMIN,
+                    )
+                    db.session.add(obj)
+                    db.session.commit()
+                    obj = obj.to_json()
+                obj = db.session.execute(
+                    db.select(User).where(User.role == Role.USER)
+                ).scalar()
+                if not obj:
+                    obj = User(
+                        name="user",
+                        surname="user",
+                        email="user@user.ru",
+                        password="user",
+                        role=Role.USER,
+                    )
+                    db.session.add(obj)
+                    db.session.commit()
+                    obj = obj.to_json()
+                    tries = 0
+            except:
+                tries += -1
+                print('Failed to create Database, ADMIN and USER. Waiting and then trying again (try countdown: %s)' % tries)
+                time.sleep(5000)
+        print("Base USER and ADMIN existed")
